514c4640660808d89c730ee9dc7510246c3c3cf8
root <bob-de-ruiter@live.nl>
2016-07-27 17:41:10 -0400

    generator based

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var alasql = require('../alasql')
+var fs = require('fs')
 
 var pretty = require('js-object-pretty-print').pretty
 var toString = obj => pretty(obj, undefined, undefined, true)
 
+var getSchema = fs.readFileSync('./getschema.sql', 'utf8')
+
 function helpers(userhelpers) {
   // patching for serialization
-  var global = alasql.utils.global
+  var global = alasql.utils.global, fn = alasql.fn
   alasql.stdfn.CHAR = function(x) { return String.fromCharCode(x) }
   alasql.utils.global = null
+  alasql.fn = userhelpers
 
   var helperString = toString({stdfn: alasql.stdfn, utils: alasql.utils, fn: alasql.fn})
 
-  alasql.utils.global = global 
+  alasql.utils.global = global
+  alasql.fn = fn
 
   return helperString
 }
@@ -98,33 +103,106 @@ function validate(tablename, data, opts) {
 
 var validator = validate.toString()
 
-module.exports = function(sql, opts) {
-  if (opts.fn) {
-    alasql.fn = opts.fn
+module.exports.fromSQL = function(sql, outopts) {
+  if (outopts.fn) {
+    alasql.fn = outopts.fn
   }
+
+  var tables = {}, checks = {}
+
   alasql(sql)
 
-  var tables = {}
-  for (name in alasql.tables) {
+  for (var name in alasql.tables) {
     var t = alasql.tables[name]
-    var defaultfns = 'return {'+t.defaultfns+'}';
-    var checks = {}, columns = {}
-    for (var i = 0; i < t.columns.length; i++) {
-      columns[t.columns[i].id] = t.columns[i]
+
+    for (var column of t.columns) {
+      columns[column.id] = {
+        notnull: column.notnull,
+        checks: []
+      }
     }
-    for (var i = 0; i < t.checks.length; i++) {
-      checks[t.checks[i].id] = t.checks[i].fn
+    for (var check of t.checks) {
+      checks[check.id] = check.fn
+      var fn = check.fn.toString(),
+        match, pattern = /r\['(.*?)'\]/g
+
+        while (match = pattern.exec(fn)) {
+          if (columns[match[1]]) {
+            columns[match[1]].checks.push(check.id)
+          }
+        }
     }
     tables[name] = {columns: t.columns, checks: checks }
-    if (t.defaultfns)
+
+    if (t.defaultfns) {
+      var defaultfns = 'return {'+t.defaultfns+'}';
       tables[name].defaults = new Function('r,db,params', defaultfns)
-  }
-
-  var exports = opts.embedValidator ? validate : '{}'
-
-  var _module = 'var tables, alasql = ' + helpers() + '\n\n'
-  _module += 'module.exports = ' + exports + '\n\n'
-  _module += 'module.exports.default = module.exports\n\n'
-  _module += 'tables = module.exports.tables = ' + toString(tables)
-  return _module
+    }
+  } 
+  return {tables, checks}
 }
+
+module.exports.fromPG = function(client, outopts, cb) {
+  var query = (sql, cb) => {
+    client.query(sql, (err, res) => {
+      if (err) {
+        throw err
+      }
+      if (res.rows) {
+        res = res.rows
+      }
+      gen.next(res)
+    })
+  }
+  var gen = (function*() {
+    var tables = {}, checks = {}
+    var columns = yield query(getSchema)
+    console.log(columns)
+    
+    for (var column of columns) {
+      var tablename = column.table_name
+      if (!tables[tablename]) {
+        tables[tablename] = {columns: {}, checks: []}
+      }
+      tables[tablename].columns[column.column_name] = {
+        default: column.column_default,
+        notnull: !column.is_nullable,
+        checks: []
+      }
+    }
+
+    var table_checks = yield query(`select *
+    from information_schema.table_constraints
+    where table_schema='public' and constraint_type='CHECK' and constraint_name not like '%\_not\_null'`)
+
+    for (var check of table_checks) {
+      var tablename = check.table_name
+      tables[tablename].checks.push(check.constraint_name)
+    }
+
+    var column_checks = yield query(`select *
+    from information_schema.constraint_column_usage
+    where table_schema = 'public'`)
+
+    for (var check of column_checks) {
+      var tablename = check.table_name
+      tables[tablename].columns[check.column_name].checks.push(check.constraint_name)
+    }
+
+    var checks = yield query(`select *
+    from information_schema.check_constraints
+    where constraint_schema = 'public' and constraint_name not like '%\_not\_null'`)
+
+    for (var check of checks) {
+      checks[check.constraint_name] = check.check_clause
+    }
+
+    cb({tables, checks})
+  })()
+  gen.next()
+}
+
+//return `var tables, alasql = ${helpers()}
+//  module.exports = ${exports}
+//  module.exports.default = module.exports
+//  tables = module.exports.tables = ${toString(tables)}`
