2f52ac8f744932ac7e5f3cbd2ff6525b49b171f4
root <bob-de-ruiter@live.nl>
2016-08-28 14:06:21 -0400

    api overhaul

--- schema.js
+++ schema.js
@@ -6,33 +6,52 @@ var extend = helpers.utils.extend, merge = function(a, b) {
   return c
 }
 
-var Table = module.exports.Table = function(schema, name) {
+var Table = function(schema, name, opts, helpers) {
   this.schema = schema
   this.name = name
+  this.opts = opts
+  this.helpers = helpers
 }
 
-var Schema = module.exports = function(v, opts) {
-  v = [].concat(v)
-  this.v = {checks: {}, tables: {}}
-  for (var i = 0; i < v.length; i++) {
-    this.extend(v[i])
-  }
-  this.helpers = extend({}, helpers)
-  this.helpers.fn = {}
-  this.opts = opts || {}
+var Schema = module.exports = function(schemas, opts) {
+  schemas = [].concat(schemas)
+  var schemaHelpers = extend({}, helpers)
+  schemaHelpers.fn = {}
+  opts = opts || {}
+  var schema = {checks: {}, tables: {}}
 
-  return function(tableName) {
-    return new Table(this, tableName)
+  var self = function(tableName) {
+    return new Table(schema, tableName, opts, schemaHelpers)
   }
+
+  self.extend = function(newschema) {
+    extend(schema.checks, newschema.checks)
+    extend(schema.tables, newschema.tables)
+  }
+
+  for (var i = 0; i < schemas.length; i++) {
+    self.extend(schemas[i])
+  }
+
+  self.functions = function(fns) {
+    extend(schemaHelpers.fn, fns)
+  }
+  self.options = function(_opts) {
+    extend(opts, _opts)
+  }
+  self.validateCheckConstraint = function(constraint, data) {
+    if (schema.checks[constraint]) {
+      return {success: schema.checks[constraint](data, schemaHelpers)}
+    }
+    else {
+      return {error: 'constraint_missing', constraint: constraint}
+    }
+  }
+  return self
 }
-Schema.prototype.functions = function(fns) {
-  extend(this.helpers.fn, fns)
-}
-Schema.prototype.options = function(opts) {
-  extend(this.opts, opts)
-}
-Schema.prototype.defaults = function(table_name, column, data) {
-  var t = this.v.tables[table_name]
+
+Table.prototype.defaults = function(column, data) {
+  var t = this.schema.tables[this.name]
   if (typeof column != 'string') {
     data = column
     column = null
@@ -48,32 +67,20 @@ Schema.prototype.defaults = function(table_name, column, data) {
   var defaults = t.defaults ? t.defaults(data) : {}
   for (column in t.columns) {
     if (!(column in defaults)) {
-      defaults[column] = t.columns[column].default ? t.columns[column].default(data, this.helpers) : undefined
+      defaults[column] = t.columns[column].default ? t.columns[column].default(data, this.helpers) : null
     }
   }
   return defaults
 }
-Schema.prototype.validateCheckConstraint = function(constraint, data) {
-  if (this.v.checks[constraint]) {
-    return {success: this.v.checks[constraint](data, this.helpers)}
-  }
-  else {
-    return {error: 'constraint_missing', constraint: constraint}
-  }
-}
-Schema.prototype.extend = function(schema) {
-  extend(this.v.checks, schema.checks)
-  extend(this.v.tables, schema.tables)
-}
-Schema.prototype.validate = function(table, data, opts) {
-  var t = this.v.tables[table]
+Table.prototype.validate = function(data, opts) {
+  var t = this.schema.tables[this.name]
 
   opts = merge(this.opts, opts || {}) 
 
   if (!t) {
     return {error: 'table_missing'}
   }
-  if (!opts.unknown) {
+  if (opts.unknown !== false) {
     for (var field in data) {
       if (!t.columns[field]) {
         return {error: 'unknown_field', violated: [field]}
@@ -90,7 +97,7 @@ Schema.prototype.validate = function(table, data, opts) {
     checkobj = opts.checks
   }
 
-  if (opts.empty) {
+  if (opts.empty === false) {
     for (column in t.columns) {
       if (data[column] == null || data[column] === '') {
         var checks = t.columns[column].checks
@@ -113,14 +120,14 @@ Schema.prototype.validate = function(table, data, opts) {
   }
 
   if (opts.defaults !== false) {
-    var defaults = this.defaults(table, void 0, data)
+    var defaults = this.defaults(void 0, data)
     data = extend(defaults, data)
   }
 
 
   var violated = []
 
-  if (!opts.empty) {
+  if (opts.empty === false) {
     for (var column in t.columns) {
       if ((!opts.columns || opts.columns.indexOf(column) > -1) && t.columns[column].notnull && (data[column] == null || opts.notNullNotEmpty && data[column] === '')) {
         violated.push(column + '_not_null')
@@ -132,7 +139,7 @@ Schema.prototype.validate = function(table, data, opts) {
   }
 
   for (var i = 0; i < checklist.length; i++) {
-    var check_name = checklist[i], check = this.v.checks[check_name]
+    var check_name = checklist[i], check = this.schema.checks[check_name]
 
     if (!check) {
       return {error: 'constraint_missing', constraint: check_name}
@@ -154,7 +161,7 @@ Schema.prototype.validate = function(table, data, opts) {
   return {success: true}
 }
 
-Schema.prototype.assert = function() {
+Table.prototype.assert = function() {
   var v = this.validate.apply(this, arguments)
   if (v.error) {
     var name = v.violated ? v.error + ': ' + v.violated.join(', ') : v.error
@@ -164,12 +171,4 @@ Schema.prototype.assert = function() {
   }
 }
 
-var addTableMethod = function(methodName) {
-  Table.prototype[methodName] = function(a1, a2, a3) {
-    this.schema[methodName](this.name, a1, a2, a3)
-  }
-}
-
-addTableMethod('defaults')
-addTableMethod('validate')
-addTableMethod('assert')
+module.exports.Table = Table
--- test.js
+++ test.js
@@ -7,20 +7,16 @@ var client = {user: 'postgres', password: '12zxcv', database: 'SLAPTCHA'}
 
 // console.log(validate('users', {email: 'a@b', password: '1'.repeat(64)}, {ignoreDefaults: 0}))
 eval(v('create table cool(a text check (a = "c"))').replace(/v-js/g, '.'))
-console.log(module.exports.validate('cool', {a: 2}))
+console.log(module.exports('cool', {}).validate({a: 'c'}))
 
-v('create table w (a text check (a = 3))')
+//v('create table w (a text check (a = 3))')
 
 var mod = v({client, sql: 'create table cool(a text not null)', tables: {
   users: ['email', 'password']
 }}, (err, res) => {
   eval(res.replace(/v-js/g, '.'))
   var schema = module.exports
-  console.log(schema.validate('users', {email: 'a@a', password: '1234567'}, {}))
-  try {
-    schema.assert('cool', {})
-  } catch (e) {
-    console.log(e.violated)
-  }
-  console.log(schema.defaults('cool'))
+  console.log(schema('users').validate({email: 'a@a', password: '1234567'}, {}))
+  schema('cool').assert({}, {unknown: false})
+  console.log(schema('cool').defaults())
 })
